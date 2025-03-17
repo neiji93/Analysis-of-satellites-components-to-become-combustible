e286da2050c263472c7caf1961df9b55ed1aad3a
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-17 12:03:04 +0100

    Create MainLifetime.hlsl

---
+++ MainLifetime.hlsl
@@ -0,0 +1,9 @@
+
+
+//errors dont propagate in the components. So the lifetime will be not clamped.
+//So use hemisphere sampling or sphere sampling.
+void svbrdf...
+
+void tdbrdf....
+
+//the GRF is not needed.
